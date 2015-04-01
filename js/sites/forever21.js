1677df661dc75fbf098b268113494dea68c97550
Ryan D'Souza <ryan.dsouza@hotmail.ca>
2015-04-01 00:27:16 -0400

    another level

--- js/sites/forever21.js
+++ js/sites/forever21.js
@@ -73,14 +73,49 @@ module.exports = new function() {
 		function(input) {
 			return new LambdaNode("Forever21 site inner category", input, function(input, scanEvents, node) {
 				node.downloadTemplate(input, scanEvents, function(body) {
-					//var parsedHTML = $.load(body);
-					//var childList = parsedHTML("#divNav .columns a").map(function(i, x) { 
-					//	return (nodes[3])({
-					//		data : $(x).attr("href")
-					//	}); 
-					//});
-					var childList = [];
+					var parsedHTML = $.load(body);
+					
+					// get id
+					var categoryId = parsedHTML("body").attr("onunload").match(/'(\d+)'/)[1]; 
+
+					// get more id's
+					var objStr = parsedHTML("head > script:not([src])").eq(0).html().match(/({(\s+|"[a-zA-Z]+":'(-?\d+|[a-z]+)?',?)+})/)[0];
+					var objStr = objStr.replace(/'/g, "\"");
+					var dataOBJ = JSON.parse(objStr); 
+					var storeId=dataOBJ.storeId, catalogId=dataOBJ.catalogId, langId=dataOBJ.langId;
+					
+					// calculate maximum page
+					var attr = parsedHTML("body div.paging_controls").eq(0).find("li.hoverover > a").last().attr("onclick");
+					var attr = attr.match(/pageNumber:"(\d+)",pageSize:"(\d+)"/);
+					var maxpageNumber = attr[1], pageSize = attr[2];
+					var maxItems = maxpageNumber * pageSize;
+					
+					// create child node for each page
+					var i, childList=[], perPage = 120;
+					for(i=0; i<maxItems; i+=perPage) {
+						var link = "http://www.forever21.com/shop/CategoryNavigationResultsView?langId="+langId+"&catalogId="+catalogId+"&categoryId="+categoryId+"&storeId="+storeId+"&beginIndex="+i+"&pageSize="+perPage;
+						childList.push((nodes[4])({
+							data : link
+						}));
+					}
+
 					return childList;
+				});
+			});
+		},
+		function(input) {
+			return new LambdaNode("Forever21 site page", input, function(input, scanEvents, node) {
+				node.downloadTemplate(input, scanEvents, function(body) {
+					/*
+					var parsedHTML = $.load(body);
+					var childList = parsedHTML("div.mdrop_column.columns.department_2 > ul > li > a:not([onclick])").map(function(i, x) { 
+						return (nodes[3])({
+							data : $(x).attr("href")
+						}); 
+					});
+					return childList;
+					*/
+					return [];
 				});
 			});
 		}
