a7091fb25801c3b2b0196eec21e2f8888df3a3f6
Ryan D'Souza <ryan.dsouza@hotmail.ca>
2015-04-26 03:17:30 -0400

    updated modules

--- crosslabelpostgresdbcode.sql
+++ crosslabelpostgresdbcode.sql
@@ -1,6 +1,60 @@
-DROP TABLE IF EXISTS product, human_demographic, product_image, retailer, variation_size, category, social_media, retailer_social_media, human_demographic_category, product_variation, variation_stock, city, country;
+drop schema public cascade;
+create schema public;
+	
+CREATE TABLE "categories" (
+	"id" integer,
+	"name" VARCHAR(255),
+	CONSTRAINT Categories_pk PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
 
-CREATE TABLE "product" (
+CREATE TABLE "social_media" (
+	"id" integer,
+	"name" VARCHAR(255),
+	CONSTRAINT Social_Media_pk PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "human_demographics" (
+	"id" integer,
+	"name" VARCHAR(255),
+	CONSTRAINT Human_Demographics_pk PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "countries" (
+	"id" integer,
+	"country" VARCHAR(255),
+	CONSTRAINT Countries_pk PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "cities" (
+	"id" integer,
+	"city" VARCHAR(255),
+	"country_id" integer,
+	CONSTRAINT Cities_pk PRIMARY KEY (id),
+	CONSTRAINT Cities_fk0 FOREIGN KEY (country_id) REFERENCES countries(id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "retailers" (
+	"id" integer,
+	"name" VARCHAR(255),
+	"homepage_link" TEXT,
+	"city_id" integer,
+	CONSTRAINT Retailers_pk PRIMARY KEY (id),
+	CONSTRAINT Retailers_fk0 FOREIGN KEY (city_id) REFERENCES cities(id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "products" (
 	"id" integer,
 	"origin_id" VARCHAR(255),
 	"category_id" integer,
@@ -12,73 +66,14 @@ CREATE TABLE "product" (
 	"retailer_id" integer,
 	"active" BOOLEAN,
 	"upvotes" integer,
-	CONSTRAINT Product_pk PRIMARY KEY (id)
+	CONSTRAINT Products_pk PRIMARY KEY (id),
+	CONSTRAINT Products_fk0 FOREIGN KEY (category_id) REFERENCES categories(id),
+	CONSTRAINT Products_fk1 FOREIGN KEY (retailer_id) REFERENCES retailers(id)
 ) WITH (
   OIDS=FALSE
 );
 
-CREATE TABLE "human_demographic" (
-	"id" integer,
-	"name" VARCHAR(255),
-	CONSTRAINT Human_Demographic_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE "human_demographic_category" (
-	"human_demographic_id" integer,
-	"category_id" integer,
-	CONSTRAINT Human_Demographic_Category_pk PRIMARY KEY(human_demographic_id, category_id)
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE "product_image" (
-	"id" integer,
-	"product_variation_id" integer,
-	"filepath" TEXT,
-	CONSTRAINT Product_Image_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE "category" (
-	"id" integer,
-	"name" VARCHAR(255),
-	CONSTRAINT Category_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE "retailer" (
-	"id" integer,
-	"name" VARCHAR(255),
-	"homepage_link" TEXT,
-	"city_id" integer,
-	CONSTRAINT Retailer_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
-);
-
-
-CREATE TABLE "social_media" (
-	"id" integer,
-	"name" VARCHAR(255),
-	CONSTRAINT Social_Media_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE "retailer_social_media" (
-	"social_media_id" integer,
-	"retailer_id" integer,
-	"social_media_link" TEXT,
-	CONSTRAINT Retailer_Social_Media_pk PRIMARY KEY (social_media_id,retailer_id)
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE "product_variation" (
+CREATE TABLE "product_variations" (
 	"id" integer,
 	"product_id" integer,
 	"origin_id" VARCHAR(255),
@@ -86,55 +81,57 @@ CREATE TABLE "product_variation" (
 	"swatch_filepath" TEXT,
 	"created_at" timestamp,
 	"updated_at" timestamp,
-	CONSTRAINT Product_Variation_pk PRIMARY KEY (id)
+	CONSTRAINT Product_Variations_pk PRIMARY KEY (id),
+	CONSTRAINT Product_Variations_fk0 FOREIGN KEY (product_id) REFERENCES products(id)
 ) WITH (
   OIDS=FALSE
 );
 
-CREATE TABLE "variation_size" (
+CREATE TABLE "human_demographics_categories" (
+	"human_demographic_id" integer,
+	"category_id" integer,
+	CONSTRAINT Human_Demographic_Categories_pk PRIMARY KEY(human_demographic_id, category_id),
+	CONSTRAINT Human_Demographic_Categories_fk0 FOREIGN KEY (human_demographic_id) REFERENCES human_demographics(id),
+	CONSTRAINT Human_Demographic_Categories_fk1 FOREIGN KEY (category_id) REFERENCES categories(id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "product_images" (
+	"id" integer,
 	"product_variation_id" integer,
-	"size" VARCHAR(255)
+	"filepath" TEXT,
+	CONSTRAINT Product_Images_pk PRIMARY KEY (id),
+	CONSTRAINT Product_Images_fk0 FOREIGN KEY (product_variation_id) REFERENCES product_variations(id)
 ) WITH (
   OIDS=FALSE
 );
 
+CREATE TABLE "retailers_social_media" (
+	"social_media_id" integer,
+	"retailer_id" integer,
+	"social_media_link" TEXT,
+	CONSTRAINT Retailers_Social_Media_pk PRIMARY KEY (social_media_id,retailer_id),
+	CONSTRAINT Retailers_Social_Media_fk0 FOREIGN KEY (social_media_id) REFERENCES social_media(id),
+	CONSTRAINT Retailers_Social_Media_fk1 FOREIGN KEY (retailer_id) REFERENCES retailers(id)
+) WITH (
+  OIDS=FALSE
+);
 
-CREATE TABLE "variation_stock" (
+CREATE TABLE "variation_sizes" (
+	"product_variation_id" integer,
+	"size" VARCHAR(255),
+	CONSTRAINT Variation_Sizes_fk0 FOREIGN KEY (product_variation_id) REFERENCES product_variations(id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE "variation_stocks" (
 	"product_variation_id" integer,
 	"min" integer,
 	"max" integer,
-	"has_more" BOOLEAN
+	"has_more" BOOLEAN,
+	CONSTRAINT Variation_Stocks_fk0 FOREIGN KEY (product_variation_id) REFERENCES product_variations(id)
 ) WITH (
   OIDS=FALSE
 );
-
-CREATE TABLE "city" (
-	"id" integer,
-	"city" VARCHAR(255),
-	"country_id" integer,
-	CONSTRAINT City_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE "country" (
-	"id" integer,
-	"country" VARCHAR(255),
-	CONSTRAINT Country_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
-);
-
-
-ALTER TABLE "human_demographic_category" ADD CONSTRAINT Human_Demographic_Category_fk0 FOREIGN KEY (human_demographic_id) REFERENCES human_demographic(id);
-ALTER TABLE "human_demographic_category" ADD CONSTRAINT Human_Demographic_Category_fk1 FOREIGN KEY (category_id) REFERENCES category(id);
-ALTER TABLE "product" ADD CONSTRAINT Product_fk1 FOREIGN KEY (category_id) REFERENCES category(id);
-ALTER TABLE "product" ADD CONSTRAINT Product_fk2 FOREIGN KEY (retailer_id) REFERENCES retailer(id);
-ALTER TABLE "product_image" ADD CONSTRAINT Product_Image_fk0 FOREIGN KEY (product_variation_id) REFERENCES product_variation(id);
-ALTER TABLE "retailer" ADD CONSTRAINT Retailer_fk0 FOREIGN KEY (city_id) REFERENCES city(id);
-ALTER TABLE "variation_size" ADD CONSTRAINT Variation_Size_fk0 FOREIGN KEY (product_variation_id) REFERENCES product_variation(id);
-ALTER TABLE "retailer_social_media" ADD CONSTRAINT Retailer_Social_Media_fk0 FOREIGN KEY (social_media_id) REFERENCES social_Media(id);
-ALTER TABLE "retailer_social_media" ADD CONSTRAINT Retailer_Social_Media_fk1 FOREIGN KEY (retailer_id) REFERENCES retailer(id);
-ALTER TABLE "product_variation" ADD CONSTRAINT Product_Variation_fk0 FOREIGN KEY (product_id) REFERENCES product(id);
-ALTER TABLE "variation_stock" ADD CONSTRAINT Variation_Stock_fk0 FOREIGN KEY (product_variation_id) REFERENCES product_variation(id);
-ALTER TABLE "city" ADD CONSTRAINT City_fk0 FOREIGN KEY (country_id) REFERENCES country(id);
